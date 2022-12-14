80836e08eaf361b934d51c8c45abcd465b6d0e4e
BerkKilicoglu <klcogluberk@gmail.com>
2022-12-14 17:26:32 +0300

    Updated deprecated param.

--- grid_search.py
+++ grid_search.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn import ensemble
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
 from time import time
@@ -45,7 +45,7 @@ def report(results, n_top=3):
 
 
 #Create the model
-model = ensemble.GradientBoostingRegressor()
+model = ensemble.GradientBoostingClassifier()
 
 #Parameters we want to try
 param_grid = {
@@ -53,9 +53,9 @@ param_grid = {
     'max_depth': [3, 6,15],          #3
     'min_samples_leaf': [1, 2, 4, 15],       #default=1
     'learning_rate': [0.05], #0.1
-    #'max_features': [0.1, 0.3,0.5,1.0], #n_features=auto
+   # 'max_features': [0.1, 0.3,0.5,1.0], #n_features=auto
     'min_samples_split': [2, 5 ,15],      #2
-    'loss': ['ls', 'lad', 'huber'],
+    'loss': ['log_loss'],
     'subsample': [ 0.4, 0.3] #1.0
 }
 
@@ -73,12 +73,14 @@ print("GridSearchCV took %.2f seconds for %d candidate parameter settings."
 report(grid_search.cv_results_)
 
 #Print the parameters that gave us the best result!
-print(grid_search.best_params_)
+print("The parameters of the best result:", grid_search.best_params_)
 
-# Find the error rate on the training set using the best parameters
-mse = mean_absolute_error(y_train, grid_search.predict(X_train))
-print("Training Set Mean Absolute Error: %.4f" % mse)
 
-# Find the error rate on the test set using the best parameters
-mse = mean_absolute_error(y_test, grid_search.predict(X_test))
-print("Test Set Mean Absolute Error: %.4f" % mse)
+# Find the accuracy rate on the training set
+acc = accuracy_score(y_train, grid_search.predict(X_train))
+print("\nTraining Set Accuracy_score: %.4f" % acc)
+
+
+# Find the accuracy rate on the test set
+acc = accuracy_score(y_test, grid_search.predict(X_test))
+print("Test Set Accuracy_score: %.4f" % acc)
--- train_model.py
+++ train_model.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import ensemble
-from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import accuracy_score, confusion_matrix
 import joblib
 from sklearn.preprocessing import LabelEncoder
 
@@ -29,14 +29,14 @@ y = df['class'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
 
 #Fit model
-model = ensemble.GradientBoostingRegressor(
+model = ensemble.GradientBoostingClassifier(
     n_estimators=700,
     learning_rate=0.05,
     max_depth=15,
     min_samples_leaf=8,
     min_samples_split=3,
     #max_features=1.0,
-    loss='huber',
+    loss='log_loss',
     subsample=0.5
 )
 model.fit(X_train, y_train)
@@ -44,11 +44,15 @@ model.fit(X_train, y_train)
 #Save the trained model to a file so we can use it in other programs
 joblib.dump(model, 'mymodel.pkl')
 
-# Find the error rate on the training set
-mse = mean_absolute_error(y_train, model.predict(X_train))
-print("Training Set Mean Absolute Error: %.4f" % mse)
+# Find the accuracy rate on the training set
+acc = accuracy_score(y_train, model.predict(X_train))
+print("Training Set Accuracy_score: %.4f" % acc)
 
-# Find the error rate on the test set
-mse = mean_absolute_error(y_test, model.predict(X_test))
-print("Test Set Mean Absolute Error: %.4f" % mse)
 
+# Find the accuracy rate on the test set
+acc = accuracy_score(y_test, model.predict(X_test))
+print("Test Set Accuracy_score: %.4f" % acc)
+
+# Print Confusion Matrix
+conf_matrix = confusion_matrix(y_test, model.predict(X_test))
+print("\n__Confusion Matrix__\n ", conf_matrix)
